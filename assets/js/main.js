05097b996e2ba0a0719ce2cf121147078a92e471
nanweb <webnancy105@gmail.com>
2023-04-26 16:12:30 -0300

    agregue loader al inicio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", () =>{
+    const contenedor_loader = document.querySelector(".contenedor_loader")
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = "hidden"
+})
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
